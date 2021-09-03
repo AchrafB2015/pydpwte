451993e12c7ef53607c3b9e0d29425c806e6f3be
bennisAch <62178971+AchrafB2015@users.noreply.github.com>
2021-09-03 14:30:22 +0200

    Update postprocess.py

--- pydpwte/utils/postprocess.py
+++ pydpwte/utils/postprocess.py
@@ -8,19 +8,34 @@ import numpy as np
 
 
 def select_alphas_and_normalize(alphas, w_th):
-    condition = alphas > w_th
-    selected_alphas = alphas[condition]
-    selected_indices = condition.flatten().numpy()
+    
+    """Post-Training Steps (cf Sect 6.3 of DPWTE paper)
+    This function takes the softmax outputs and the threshold (above which alpha_k is selected) and returns the 
+    list of alpha_k selected as well as their respective indices.
+    Argument:
+        alphas {tensor}: weighting coefficients of the Weibull distributions learned by the network.
+        w_th {float}   : the weight's threshold.
+    """
+    condition              = alphas > w_th
+    selected_alphas        = alphas[condition]
+    selected_indices       = condition.flatten().numpy()
     sum_of_selected_alphas = selected_alphas.sum()
-    selected_alphas /= sum_of_selected_alphas
+    selected_alphas       /= sum_of_selected_alphas
     return list(selected_alphas), selected_indices
 
 
 def select_mixture_parameters(results, selected_indices):
-    beta_pred = results[0] + 2
-    eta_pred = results[1] + 1 + 1e-4
-    alpha_pred = results[2]
-    selected_beta = beta_pred[:, selected_indices].detach().numpy()
-    selected_eta = eta_pred[:, selected_indices].detach().numpy()
+    
+    """ Recover the real values of the learned parameters and convert them to arrays.
+    Argument:
+        results {list}: the set of triplets (beta_k,eta_k,alpha_k).
+        selected_indices {array}: the respective indices of the selected alphas (selected Weibulls).
+    """
+    
+    beta_pred      = results[0] + 2
+    eta_pred       = results[1] + 1 + 1e-4
+    alpha_pred     = results[2]
+    selected_beta  = beta_pred[:, selected_indices].detach().numpy()
+    selected_eta   = eta_pred[:, selected_indices].detach().numpy()
     selected_alpha = alpha_pred[:, selected_indices].detach().numpy()
     return np.array(selected_beta), np.array(selected_eta), np.array(selected_alpha)
